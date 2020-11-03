a07543e0b2502908157e2727b1fb2e77a043d098
Joel Ala-aho <43337796+Joqz@users.noreply.github.com>
2020-11-03 11:40:59 +0200

    Test game commit

---
+++ Assets/Destroyable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyable : MonoBehaviour
+{
+    public int health = 100;
+
+    public GameObject destroyEffect;
+
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        //Instantiate(destroyEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+}
